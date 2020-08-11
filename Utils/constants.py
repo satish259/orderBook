0ed58000a52f63d1d44ba2288f2c0b3ebd8e99fc
satish259 <30528003+satish259@users.noreply.github.com>
2020-08-11 22:27:19 +0100

    Add files via upload
    Minor refactoring of constants

--- Utils/constants.py
+++ Utils/constants.py
@@ -3,3 +3,5 @@
 
 orderKeysAdd = ['timestamp', 'orderId', 'action', 'ticker', 'side', 'price', 'quantity']  # Fields for an new order
 orderKeys = ['timestamp', 'orderId', 'action']  # minimum required order fields to handle all orders
+
+BuySell=['B', 'S']
