729831b4535b777a6335021ad9027f0a4f3c06bc
satish259 <30528003+satish259@users.noreply.github.com>
2020-08-11 22:17:25 +0100

    Update test_OrderBook.py
    Minor refactoring

--- test_OrderBook.py
+++ test_OrderBook.py
@@ -2,13 +2,15 @@
 import pytest
 from OrderBook import OrderBook, processOrder, getBestBidAndAsk
 
+ob = OrderBook()
+
 
 def test__add_order():
     """
     This is a perfect case!
     New order gets created successfully.
     """
-    ob = OrderBook()
+
     processOrder(ob, '1568390243|abbb11|a|AAPL|B|209.00000|100')
     assert getBestBidAndAsk(ob, 'AAPL') == '209.0/0.0'
 
@@ -18,7 +20,6 @@ def test__cancel_order():
     This is a perfect case!
     Order gets cancelled properly.
     """
-    ob = OrderBook()
     processOrder(ob, '1568390243|abbb11|a|AAPL|B|209.00000|100')
     # Intentional incorrect cancel order as other colmns are ignores
     processOrder(ob, '1568390245|abbb11|c|AAPL|B|209.00000|100')
@@ -30,43 +31,42 @@ def test__cancel_order_error():
     Cancelling an order that does not exist.
     I choose to throw an error but the code can be changed to ignore it too!
     """
-    ob = OrderBook()
     with pytest.raises(ValueError,
                        match="Incorrect/invalid orderId with value abbb11. Unable to cancel order as it does not exist."):
         processOrder(ob, '1568390247|abbb11|c|AAPL|B|209.00000|100')
+
 
 def test_max_bid_ask():
     """
     The perfect case is covered by other tests so covering zero case.
     """
-    ob = OrderBook()
     assert getBestBidAndAsk(ob, '') == '0.0/0.0'
 
+
 def test__update_order():
-    ob = OrderBook()
     processOrder(ob, '1568390243|abbb11|a|AAPL|B|209.00000|100')
     # This covers changing ticker, side, price and quantity
     processOrder(ob, '1568390247|abbb11|u|JPM|S|101.00000|1')
     assert ob.orders['abbb11'].ticker == 'JPM'
-    assert ob.orders['abbb11'].side=='S'
+    assert ob.orders['abbb11'].side == 'S'
     assert ob.orders['abbb11'].price == 101.0
     assert ob.orders['abbb11'].quantity == 1
+
 
 def test__update_order_error():
     """
     Updating an order that does not exist.
     I choose to throw an error but the code can be changed to treat an update without an add as an add!
     """
-    ob = OrderBook()
     with pytest.raises(ValueError,
-                       match="Incorrect/invalid orderId with value abbb11. Unable to amend order as it does not exist."):
-        processOrder(ob, '1568390247|abbb11|u|100')
+                       match="Incorrect/invalid orderId with value abbb. Unable to amend order as it does not exist."):
+        processOrder(ob, '1568390247|abbb|u|100')
+
 
 def test_process_order_error():
     """
     The perfect case is covered by other tests so covering incorrect action only.
     """
-    ob = OrderBook()
     with pytest.raises(ValueError,
                        match="Incorrect/invalid action with value K. a, u and c are the allowable actions."):
         processOrder(ob, '1568390243|abbb11|K')
